f8d7b05b860bd2010e2da4996c3456628aa37591
leonTech254 <martinleontech23@gmail.com>
2023-12-24 12:24:55 +0300

    --update login modele

--- app/src/main/java/com/leonteqsecurity/suitcasenew/LoginScreen.java
+++ app/src/main/java/com/leonteqsecurity/suitcasenew/LoginScreen.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.leonteqsecurity.suitcasenew.Database.DbHelper;
@@ -18,12 +19,20 @@ import java.util.function.IntConsumer;
 public class LoginScreen extends AppCompatActivity {
     private DbHelper dbHelper;
 
-
+    private EditText Username,Password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
         dbHelper=new DbHelper(this);
+        InitializeElement();
+    }
+
+    public  void InitializeElement()
+    {
+        Username=findViewById(R.id.username);
+        Password=findViewById(R.id.password);
+
     }
 
     public void ToRegister(View view) {
@@ -37,13 +46,32 @@ public class LoginScreen extends AppCompatActivity {
         {
             Toast.makeText(this, "The user is Already Registered Successfully. \nKindly Login to the Existing Account", Toast.LENGTH_SHORT).show();
         }
-
-
-
     }
 
     public void SignIn(View view) {
-        Intent intent=new Intent(this, MainScreen.class);
-        startActivity(intent);
+        String username=Username.getText().toString();
+        String password =Password.getText().toString();
+        if(!username.isEmpty() || !password.isEmpty())
+        {
+
+//            check user credientials
+            List<User> users=dbHelper.getAllUsers();
+            //getting the firstuser from the database
+            User firstUser= users.get(0);
+            String dbUsername=firstUser.getUsername();
+            String dbPassword=firstUser.getPassword();
+            if(username.equals(dbUsername) && dbPassword.equals(password))
+            {
+                Intent intent=new Intent(this, MainScreen.class);
+                startActivity(intent);
+
+            }else
+            {
+                Toast.makeText(this, "Wrong username and password", Toast.LENGTH_SHORT).show();
+            }
+        }else
+        {
+            Toast.makeText(this, "Username and password required", Toast.LENGTH_SHORT).show();
+        }
     }
 }
